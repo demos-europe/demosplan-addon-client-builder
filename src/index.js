3214511ed2bac808eeb2d5496d55daf0c8c45dd7
Natalija Sakuta <sakuta@demos-deutschland.de>
2025-03-26 09:27:51 +0100

    test

--- src/index.js
+++ src/index.js
@@ -82,6 +82,7 @@ function configBuilder(addon_name, entrypoints) {
         {
           test: /\.vue$/,
           loader: 'vue-loader',
+          exclude: /node_modules/,
           options: {
             compilerOptions: {
               compatConfig: {
