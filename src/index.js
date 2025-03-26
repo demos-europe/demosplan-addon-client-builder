76448160e83fa2f3cd1657f9e88a755132cfc10e
Natalija Sakuta <sakuta@demos-deutschland.de>
2025-03-26 10:01:03 +0100

    test

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ function configBuilder(addon_name, entrypoints) {
         {
           test: /\.vue$/,
           loader: 'vue-loader',
-          exclude: /node_modules\/(?!@demos-europe\/demosplan-ui)/,
+          exclude: file => /node_modules/.test(file) && /@demos-europe\/demosplan-ui/.test(file),
           options: {
             compilerOptions: {
               compatConfig: {
