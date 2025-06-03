fc8d34a38b44467c4701b54b99a1ada7f7018ac5
Natalija Sakuta <sakuta@demos-deutschland.de>
2025-06-03 17:09:02 +0200

    fix: update a comment

--- src/removeSFCBlockLoader.js
+++ src/removeSFCBlockLoader.js
@@ -2,6 +2,7 @@
  * This loader captures any <license> custom block (matched by resourceQuery: /blockType=license/).
  * Without a loader, vue-loader would throw a syntax error when encountering <license> blocks.
  * By returning an empty string here, we remove those blocks, allowing vue-loader to continue without interruption.
+ * (https://vue-loader.vuejs.org/guide/custom-blocks.html#example)
  */
 module.exports = function (source, map) {
   this.callback(
